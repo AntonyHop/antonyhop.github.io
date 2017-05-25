ad82fb25784ec6181ea3116e9a278a1a79b03ff4
AntonyHop <stargeit123@gmail.com>
2017-05-25 19:01:09 +0300

    Enden landing

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
    console.log("site is ready");
     $('.main-tail').height(window.innerHeight-$('header').height()/window.innerHeight);
+
+    date = new Date();
+    console.log(date)
+    $(".yer").text(date.getFullYear());
+    
+    $(".main-nav").click(function (elem) {
+        if ($(elem.target).data("link") != undefined){
+            console.log($(elem.target).data("link"));
+            goToByScroll($(elem.target).data("link"),200);
+        }
+    })
+
+    function goToByScroll(id,offset){
+        if (offset == undefined) offset = 0
+        if(!$("body").is(id)){
+            $('html,body').animate({scrollTop: Math.abs($(id).offset().top-offset)}, 'slow');
+        }
+    }
 });
 
