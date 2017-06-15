924763f38215657f402aa682aa23eef5c873df4e
AntonyHop <stargeit123@gmail.com>
2017-06-15 16:27:24 +0300

    Create callback form

--- js/app.js
+++ js/app.js
@@ -22,10 +22,30 @@ $(document).ready(function () {
 
     var XHR = ( window.XMLHttpRequest && "withCredentials" in (new XMLHttpRequest) ) ? new XMLHttpRequest : window.XDomainRequest ? new XDomainRequest : null;
     if(XHR){
-        XHR.open("GET","http://relaxmusic.esy.es/main/form");
-        XHR.onload = function(){
-            alert( this.responseText );
-        }
+        $("#tel").mask("(999) 999 9999");
+        $('.send-form').click(function(){
+            var send = false;
+            if ($('#name').val().length > 0 && $('#tel').val().length>0){
+                send = true;
+                $('#name').css('border','2px solid #ccc');
+                $('#tel').css('border','2px solid #ccc');
+            }else {
+                $('#name').css('border','2px solid red');
+                $('#tel').css('border','2px solid red');
+            }
+            if(send){
+                var body = 'name=' + encodeURIComponent($('#name').val()) + '&tel=' + encodeURIComponent($('#tel').val()) + '&text=' + encodeURIComponent($('#text').val());
+                XHR.open("POST","http://relaxmusic.esy.es/main/form&"+body);
+                XHR.onload = function(){
+                    if(this.responseText == 'done'){
+                        $('#add_work').modal('toggle');
+                    }
+                }
+                XHR.send();
+            }
+        });
+    }else {
+        $('.addW').hide();
     }
 
 });
